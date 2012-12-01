ed7cebf6b3b1cdcdf757537cfc5642fd7be4bff6
Denis Labaye <denis.labaye@gmail.com>
2012-12-01 16:06:08 +0100

    some docs

--- src/cloxy/core.clj
+++ src/cloxy/core.clj
@@ -13,6 +13,8 @@
              [io             :as io]]
             [clj-http.client :as client]))
 
+;; utilities ==================================================================
+
 (defn prns
   "Print a preview of a datastructure"
   ([                         s] (prns 2 s))
@@ -21,3 +23,15 @@
                                           *print-length* print-length]
                                   (pprint s))))
 
+;; http =======================================================================
+
+(def activiti-base-url "http://ec2-79-125-75-236.eu-west-1.compute.amazonaws.com:8080/activiti-rest/service")
+
+(defn activiti-q "Template for all activiti queries"
+  [method path & [opts]]
+  (let [p (merge {:debug       true
+                  :method      method
+                  :url         (str activiti-base-url path)
+                  :basic-auth  ["kermit" "kermit"]}
+                 opts)]
+    (c/request p)))
