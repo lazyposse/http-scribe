9d91a0b990344fe3f7f1b2ec2fa1d9128649fad3
Denis Labaye <denis.labaye@gmail.com>
2013-01-03 08:24:22 +0100

    fake server script

--- resource/fake-serve-on-8080.sh
+++ resource/fake-serve-on-8080.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -e
 
+cd "$(readlink -f "$(dirname "$0")")"
+
 while true ;
 do
     { echo -ne "HTTP/1.0 200 OK\r\nContent-Length: $(wc -c < some.file)\r\n\r\n"; cat some.file; } | nc.traditional -l -p 8080
